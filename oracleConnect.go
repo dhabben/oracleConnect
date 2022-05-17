a616f20281a4bb88eb73cb8aa044f9881a90c768
dhabben <dhabben@users.noreply.github.com>
2022-05-17 15:15:51 -0500

    Create oracleConnect.go

---
+++ oracleConnect.go
@@ -0,0 +1,26 @@
+func oracleConnect() *sql.DB {
+
+    user := os.Getenv("ORACLE_USER")
+    password := os.Getenv("ORACLE_PASS")
+    host := os.Getenv("ORACLE_HOST")
+    sid := os.Getenv("ORACLE_SID")
+
+    if user == "" || password == "" || host == "" || sid == "" {
+        fmt.Println("Please set ORACLE_USER, ORACLE_PASS, ORACLE_HOST, and ORACLE_SID first")
+        os.Exit(1)
+    }
+
+    db, err := sql.Open("godror", user+"/"+password+"@"+host+"/"+sid)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    db.SetMaxOpenConns(1)
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return db
+}
